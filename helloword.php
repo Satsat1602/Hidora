dc0ad79e2d8801398a6727a1db013fb66f4e54a3
Satsat1602 <antoine.robert@bluewin.ch>
2017-04-27 09:23:45 +0200

    Modification helloword

--- helloword.php
+++ helloword.php
@@ -4,5 +4,10 @@
  </head>
  <body>
  <?php echo '<p>Bonjour le monde</p>'; ?>
+ <p>Votre nom : <input type="text" name="nom" /> </p>
+ <p>Votre prénom : <input type="text" name="prénom" /> </p>
+ <p>Votre pseudo : <input type="text" name="pseudo" /> </p>
+ <p>Votre mot de passe: <input type="password" name="motDePasse" /> </p>
+ 
  </body>
 </html>
